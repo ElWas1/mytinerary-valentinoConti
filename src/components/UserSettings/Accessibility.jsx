8591f634855a6cca5cf62ce0ee08625dac18871d
ElWas1 <tinoxdwtf@gmail.com>
2024-02-11 13:54:01 -0300

    Added a responsive sidebar and improved components on UserSettings page

--- src/components/UserSettings/Accessibility.jsx
+++ src/components/UserSettings/Accessibility.jsx
@@ -1,6 +1,8 @@
 const Accessibility = () => {
   return (
-    <div>Accessibility</div>
+    <div className="flex flex-col items-center justify-center bg-base-300 rounded-md max-sm:p-2 sm:p-4">
+      <p className="text-xl font-medium">Accessibility</p>
+    </div>
   )
 }
 
--- src/components/UserSettings/AccountActions.jsx
+++ src/components/UserSettings/AccountActions.jsx
@@ -1,6 +1,10 @@
 const AccountActions = () => {
   return (
-    <div>AccountActions</div>
+    <div className="flex flex-col items-center justify-center">
+      <p className="text-xl font-medium mb-4 bg-base-300 rounded-md max-sm:p-2 sm:p-4">Account actions</p>
+      <p className="mb-2">Delete account</p>
+      <button className="btn bg-red-500">Delete</button>
+    </div>
   )
 }
 
